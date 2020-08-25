7510450796fa12b1b33bf0d9fb498e82dd832dd0
Michael Awad <michael.awad@rabobank.nl>
2020-08-25 23:12:22 +0200

    feature: Experimenting with different paper components

--- app/screens/TestPage/index.tsx
+++ app/screens/TestPage/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Vibration } from 'react-native';
 import {
-  Avatar,
+  Title,
   Button,
   Card,
-  Title,
-  Paragraph,
   Checkbox,
+  List,
+  Text,
+  Divider,
 } from 'react-native-paper';
 
 import NavigationService from 'app/navigation/NavigationService';
@@ -14,20 +15,87 @@ import NavigationService from 'app/navigation/NavigationService';
 import styles from './styles';
 const Home: React.FC = () => {
   const goBack = () => NavigationService.goBack();
-  const LeftContentChecked = (props) => <Avatar.Icon {...props} icon="check" />;
-  const LeftContentUnChecked = (props) => <Avatar.Icon {...props} />;
+  const DURATION = 10000;
+  const LeftContentChecked = (props) => <List.Icon {...props} icon="trophy" />;
+  const LeftContentUnChecked = (props) => <List.Icon {...props} />;
+  const LeftItemChecked = (props) => <List.Icon {...props} icon="hexagram" />;
+  const LeftItemUnChecked = (props) => (
+    <List.Icon {...props} icon="hexagram-outline" />
+  );
+  const RightCardUnChecked = (props) => (
+    <List.Icon {...props} icon="circle-outline" />
+  );
+  const RightCardChecked = (props) => (
+    <List.Icon {...props} icon="circle-slice-1" />
+  );
   const Right = (props) => (
     <Checkbox
       label="Check for completion"
       status={checked ? 'checked' : 'unchecked'}
       onPress={() => {
         setChecked(!checked);
+        Vibration.vibrate(DURATION);
       }}
     />
   );
   const [checked, setChecked] = React.useState(false);
   return (
     <View>
+      <Card>
+        <Card.Title
+          title="Today"
+          left={checked ? LeftContentChecked : LeftContentUnChecked}
+          right={checked ? RightCardChecked : RightCardUnChecked}
+        />
+        <Card.Content>
+          <Title>You can do it!</Title>
+        </Card.Content>
+      </Card>
+      <List.Item
+        title="First Item"
+        description="Item description"
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        left={checked ? LeftItemChecked : LeftItemUnChecked}
+        right={Right}
+      />
+      <List.Item
+        title="Second Item"
+        description="Item description"
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        left={checked ? LeftItemChecked : LeftItemUnChecked}
+        right={Right}
+      />
+      <List.Item
+        title="Third Item"
+        description="Item description"
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        left={checked ? LeftItemChecked : LeftItemUnChecked}
+        right={Right}
+      />
+      <List.Item
+        title="Fourth Item"
+        description="Item description"
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        left={checked ? LeftItemChecked : LeftItemUnChecked}
+        right={Right}
+      />
+      <List.Item
+        title="Fifth Item"
+        description="Item description"
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        left={checked ? LeftItemChecked : LeftItemUnChecked}
+        right={Right}
+      />
       <Button
         icon="keyboard-backspace"
         mode="outlined"
@@ -35,17 +103,6 @@ const Home: React.FC = () => {
         accessibilityStates="">
         Go Back
       </Button>
-      <Card
-        onPress={() => {
-          setChecked(!checked);
-        }}>
-        <Card.Title
-          title="Action Item 1"
-          subtitle="Start an App"
-          left={checked ? LeftContentChecked : LeftContentUnChecked}
-          right={Right}
-        />
-      </Card>
     </View>
   );
 };
